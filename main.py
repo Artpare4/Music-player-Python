1e4c3e392c2c83db9f93930f5912dd34f470c409
pare0028 <arthur.parent@etudiant.univ-reims.fr>
2023-04-19 22:30:22 +0200

    Git Init

---
+++ main.py
@@ -0,0 +1,9 @@
+from tkinter import *
+from pydub import AudioSegment
+from pydub.playback import play
+import pygame
+root=Tk()
+root.title("MP3 player")
+root.geometry("500x300")
+
+root.mainloop()
