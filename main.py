14d0e603842ffc1caaeac9365dabc32906171d6d
pare0028 <arthur.parent@etudiant.univ-reims.fr>
2023-04-21 16:36:42 +0200

    Ajout fonctionnalité: mise à jour du contenu de la listebox quand on décide de changer de répertoire

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ class main :
         self.button_position=[(340,200),(200,100)] ## Position des bouton
         self.song_list=[] ## Liste des chansons contenu dans la répertoire que l'utilisateur peut choisir
         self.song_directory=None ##Chemin d'accès au répertoire
-
+        self.listbox_musique=Listbox(selectmode="select") ## liste de choix des musique disponible
     def add_button(self,texte:str):
         """
         Méthode de la classe main. Cette méthode permet de créer un bouton avec un texte associé passé un paramètre.
@@ -50,28 +50,32 @@ class main :
         pass
     def set_empty_list_song(self):
         """
-
+        Méthode de la classe main. Cette méthode permet de vider la liste des titres des chansons disponible dans le répertoire.
         :return:
         """
         self.song_list=[]
     def set_list_song(self):
         """
-        Méthode de la classe main. Cette méthode permet de récupérer tous le fichiers .mp3 du dossier choisi par l'utilisateur sous forme d'une liste.
+        Méthode de la classe main. Cette méthode permet de récupérer tous le fichiers .mp3 ou.wav du dossier choisi par l'utilisateur sous forme d'une liste.
         Ne retoune rien et ne prend pas de paramètre.
         :return:
         """
         for file in os.listdir(self.song_directory):
-            if file.endswith(".mp3"):
+            if file.endswith(".mp3") or file.endswith(".wav"):
                 self.song_list.append(file)
-    def set_song_folder(self):
+
+    def set_song_choice(self):
         """
         Méthode de la classe main. Cette méthode récupère le chemin d'accès au dossier choisi par l'utilisateur
         et met à jour la liste de chanson disponible (que l'utilisateur peut écouter).
         :return:
         """
-        self.song_directory=filedialog.askdirectory()
-        self.set_empty_list_song()
-        self.set_list_song()
+        self.song_directory=filedialog.askdirectory() ## on demande le chemin d'accès au répertoire
+        self.set_empty_list_song() ## suppression du contenu de la liste de chanson
+        self.listbox_musique.delete(0,"end") ## suppression du contenu de la listebox
+        self.set_list_song() ## On ajoute à la liste de chanson disponible tous les sons de dossier
+        self.add_musique_listbox(self.listbox_musique) ##Ajout de la liste de musique dans la listbox
+
     def set_command_button(self,bouton:Button,methode):
         """
         Méthode de la classe main. Cette méthode prend en paramètre un bouton et une méthode et ajoute cette commande dans l'action du bouton quand il est cliqué.
@@ -82,16 +86,35 @@ class main :
         """
         bouton.configure(command=methode)
 
+    def add_musique_listbox(self,listbox:Listbox):
+        """
+        Méthode de la classe main. Cette méthode ajoute tous les titres des chansons disponible dans la listbox.
+        :param listbox:
+        :return:
+        """
+        for song in self.song_list:
+            listbox.insert("end",song)
+
     def set_all(self):
         """
         Méthode de la classe main. Cette méthode permet de tout initialiser (boutons, liste,...) avant de lancer le programme.
         Ne prend pas de paramètre et ne retourn rien.
         :return:
         """
-        button1=self.add_button("open")
-        self.set_position_button(button1,100,110)
-        self.set_command_button(button1,self.set_song_folder)
+        # Bouton de recherche de dossier
+        #button1=self.add_button("open")
+        #self.set_position_button(button1,100,110)
+        #self.set_command_button(button1,self.set_song_folder)
+        button1=Button(self.app,text="Ouvrir")
+        button1.place(x=100,y=100)
+        button1.configure(command=self.set_song_choice)
 
+        ## mise en place listebox
+        self.listbox_musique.place(x=200,y=200)
+
+        ## première recherche dans un répertoire (quand on lance l'appli)
+        self.set_song_choice()
+        self.add_musique_listbox(self.listbox_musique)
     def test(self):
         print("test Validé")
 
@@ -100,6 +123,7 @@ class main :
         Méthode de la classe main. Cette méthode permet d'exécuter l'application.
         :return:
         """
+
         self.set_all()
         self.app.mainloop()
 
